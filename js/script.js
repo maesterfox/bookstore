b526dd0803c88c0e669807707d80b50232c7112f
maesterfox <talentedmrfox@hotmail.com>
2023-09-28 23:21:29 +0100

    responsive, addtional styles, updated modals for more info

--- js/script.js
+++ js/script.js
@@ -2,66 +2,71 @@ $(document).ready(function () {
   $("#book-search-form").submit(function (event) {
     event.preventDefault();
 
+    // Fetch data from the form
     const searchQuery = $("#searchQuery").val();
 
+    // Validate the form data
     if (!searchQuery) {
       $("#messageArea").text("At least one field must be filled out.");
       return;
     }
 
+    // Google Books API URL
     const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(
       searchQuery
-    )}&AIzaSyB9Aqx8I7N-EDkzdUEPzRFyu8QdWukPDyk`;
+    )}`;
 
+    // AJAX GET Request
     $.get(apiUrl, function (data) {
       if (!data.items) {
         $("#messageArea").text("No data returned from the server");
         return;
       }
 
-      let tableContent = '<table class="table">';
+      // Create the table content
+      let tableContent = '<table class="table resultsTable">';
       tableContent +=
-        "<thead><tr><th>Cover</th><th>Title</th><th>Summary</th><th>Authors</th><th>Published Date</th></tr></thead>";
-      tableContent += "<tbody>";
+        "<thead><tr><th>Cover</th><th>Details</th></tr></thead><tbody>"; // Changed the headers
 
+      // Populate the table with book data
       data.items.forEach(function (book) {
         const volumeInfo = book.volumeInfo;
         const authors = volumeInfo.authors
           ? volumeInfo.authors.join(", ")
           : "N/A";
         const title = volumeInfo.title || "N/A";
-        const publishedDate = volumeInfo.publishedDate || "N/A";
+        const publishedDate = volumeInfo.publishedDate || "N/A"; // Added this line
         const coverImage = volumeInfo.imageLinks
           ? `<img src="${volumeInfo.imageLinks.thumbnail}" alt="Cover Image" />`
           : "N/A";
         const fullSummary = volumeInfo.description || "N/A";
-        const summary =
-          fullSummary.length > 100
-            ? `${fullSummary.substring(
-                0,
-                100
-              )}... <a href="#" class="view-summary">Read More</a><span class="hidden full-summary">${fullSummary}</span>`
-            : fullSummary;
 
         tableContent += `<tr>`;
         tableContent += `<td>${coverImage}</td>`;
-        tableContent += `<td>${title}</td>`;
-        tableContent += `<td>${summary}</td>`;
-        tableContent += `<td>${authors}</td>`;
-        tableContent += `<td>${publishedDate}</td>`;
+        tableContent += `<td><strong>Title:</strong> ${title}<br>
+                   <strong>Authors:</strong> ${authors}<br>
+                   <strong>Published Date:</strong> ${publishedDate}<br>  <!-- Added this line -->
+                   <a href="#" class="view-summary" data-summary="${fullSummary}" data-title="${title}" data-authors="${authors}" data-date="${publishedDate}">Summary</a></td>`;
         tableContent += `</tr>`;
       });
 
       tableContent += "</tbody></table>";
       $(".table-container").html(tableContent);
 
+      // Add click event for "Read More" links
       $(".view-summary").click(function (e) {
         e.preventDefault();
-        const fullSummary = $(this).siblings(".full-summary").text();
-        $("#fullSummary").text(fullSummary);
+        const fullSummary = $(this).data("summary");
+        const title = $(this).data("title");
+        const authors = $(this).data("authors");
+        const date = $(this).data("date");
+        $("#fullSummary").html(
+          `<h3>${title}</h3><h4>${authors}</h4><h5>${date}</h5><p>${fullSummary}</p>`
+        );
         $("#summaryModal").show();
       });
 
+      // Close modal
       $("#closeModal").click(function () {
         $("#summaryModal").hide();
       });
